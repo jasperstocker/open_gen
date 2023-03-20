5c2ee6a9de179983ae4046dad115782e3fbd2a88
jasperstocker <email@jasperstocker.com>
2023-03-20 18:45:47 +0000

    Add clockwise test

--- opengen/Assets/Thirdparty/OpenGen/Maths/Shapes/Clockwise.cs
+++ opengen/Assets/Thirdparty/OpenGen/Maths/Shapes/Clockwise.cs
@@ -5,141 +5,48 @@ using UnityEngine;
 
 namespace opengen.maths.shapes
 {
-    public class Clockwise
-    {
-        public static bool Check(IList<Vector2Fixed> points)
-        {
-            int numberOfPoints = points.Count;
-            int i, j, k;
-            int count = 0;
-            float z;
+	public class Clockwise
+	{
+		public static bool Check(IList<Vector2Fixed> points)
+		{
+			int numberOfPoints = points.Count;
+			if (numberOfPoints < 3)
+			{
+				return (false);
+			}
 
-            if (numberOfPoints < 3)
-            {
-	            return (false);
-            }
+			float signedArea = 0;
+			for (int i = 0; i < numberOfPoints; i++)
+			{
+				int j = (i + 1) % numberOfPoints;
+				
+				Vector2 pointA = points[i].vector2;
+				Vector2 pointB = points[j].vector2;
 
-            for (i = 0; i < numberOfPoints; i++)
-            {
-                j = (i + 1) % numberOfPoints;
-                k = (i + 2) % numberOfPoints;
+				signedArea += pointA.x * pointB.y - pointB.x * pointA.y;
+			}
+			return signedArea / 2 < 0;
+		}
 
-                Vector2 pointA = points[i].vector2;
-                Vector2 pointB = points[j].vector2;
-                Vector2 pointC = points[k].vector2;
+		public static bool Check(IList<Vector2> points)
+		{
+			int numberOfPoints = points.Count;
+			if (numberOfPoints < 3)
+			{
+				return (false);
+			}
 
-                z = (pointB.x - pointA.x) * (pointC.y - pointA.y);
-                z -= (pointB.y - pointA.y) * (pointC.x - pointA.x);
+			float signedArea = 0;
+			for (int i = 0; i < numberOfPoints; i++)
+			{
+				int j = (i + 1) % numberOfPoints;
+				
+				Vector2 pointA = points[i];
+				Vector2 pointB = points[j];
 
-                if (z < 0)
-                {
-	                count--;
-                }
-                else if (z > 0)
-                {
-	                count++;
-                }
-            }
-
-            if (count > 0)
-            {
-	            return (true);
-            }
-            else if (count < 0)
-            {
-	            return (false);
-            }
-            else
-            {
-	            return (false);
-            }
-        }
-
-        public static bool Check(IList<Vector2> points) {
-            int numberOfPoints = points.Count;
-            int i, j, k;
-            int count = 0;
-            float z;
-
-            if (numberOfPoints < 3)
-            {
-	            return (false);
-            }
-
-            for (i = 0; i < numberOfPoints; i++) {
-                j = (i + 1) % numberOfPoints;
-                k = (i + 2) % numberOfPoints;
-
-                Vector2 pointA = points[i];
-                Vector2 pointB = points[j];
-                Vector2 pointC = points[k];
-
-                z = (pointB.x - pointA.x) * (pointC.y - pointA.y);
-                z -= (pointB.y - pointA.y) * (pointC.x - pointA.x);
-
-                if (z < 0)
-                {
-	                count--;
-                }
-                else if (z > 0)
-                {
-	                count++;
-                }
-            }
-
-            if (count > 0)
-            {
-	            return (true);
-            }
-            else if (count < 0)
-            {
-	            return (false);
-            }
-            else
-            {
-	            return (false);
-            }
-        }
-        
-	    public static bool Check(IList<Vector3> points)
-	    {
-		    int numberOfPoints = points.Count;
-		    int i, j, k;
-		    int count = 0;
-		    float z;
-
-		    if (numberOfPoints < 3)
-		    {
-			    return (false);
-		    }
-
-		    for (i = 0; i < numberOfPoints; i++)
-		    {
-			    j = (i + 1) % numberOfPoints;
-			    k = (i + 2) % numberOfPoints;
-
-			    Vector3 pointA = points[i];
-			    Vector3 pointB = points[j];
-			    Vector3 pointC = points[k];
-
-			    z = (pointB.x - pointA.x) * (pointC.z - pointA.z);
-			    z -= (pointB.z - pointA.z) * (pointC.x - pointA.x);
-
-			    if (z < 0)
-			    {
-				    count--;
-			    }
-			    else if (z > 0)
-			    {
-				    count++;
-			    }
-		    }
-
-		    if (count > 0)
-		    {
-			    return (true);
-		    }
-			return (false);
-	    }
+				signedArea += pointA.x * pointB.y - pointB.x * pointA.y;
+			}
+			return signedArea / 2 < 0;
+		}
 	}
 }
---
+++ opengen/Assets/Thirdparty/OpenGen/Utils/DebugDraw.cs
@@ -0,0 +1,31 @@
+﻿using opengen.types;
+using UnityEngine;
+
+namespace opengen
+{
+	public static class DebugDraw
+	{
+		public static void DrawShape(Vector2[] shape, Color colour, float duration = 0.0f)
+		{
+			DrawShape(shape, Vector3.zero, colour, duration);
+		}
+		
+		public static void DrawShape(Vector2[] shape, Vector3 offset, Color colour, float duration = 0.0f)
+		{
+			int shapeSize = shape.Length;
+			for(int i = 0; i < shapeSize; i++)
+			{
+				int next = i + 1;
+				if(next >= shapeSize)
+				{
+					next = 0;
+				}
+
+				Vector3 p0 = shape[i].Vector3Flat() + offset;
+				Vector3 p1 = shape[next].Vector3Flat() + offset;
+
+				Debug.DrawLine(p0, p1, colour, duration);
+			}
+		}
+	}
+}
